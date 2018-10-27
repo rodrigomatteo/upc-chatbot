1a6d1110b40a16d50b1ad66e46e4bf3e7bdb80b4
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-10-27 15:11:37 -0500

    Agregar archivos de proyecto.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=upc-chatbot-Web-Deploy.pubxml -p:Password=xl2k0yjkwgglA9AdZnt5TQQgM0NjJt7hsdlByP6KRqaa81THQiHyhPcMe3Lf
+
