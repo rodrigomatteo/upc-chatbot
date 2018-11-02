cdf0099a2f6447490a3c3d1ab472ea8a19917645
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-11-02 17:56:35 -0500

    Update Controller - Saludo Bienvenida

--- Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
+++ Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
@@ -1,6 +1,5 @@
 using System.Threading.Tasks;
 using System.Web.Http;
-
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.Dialogs;
 using System.Web.Http.Description;
@@ -36,81 +35,39 @@ namespace Microsoft.Bot.Sample.FormBot
 
         #endregion
 
+
         /// <summary>
-        /// POST: api/Messages
-        /// receive a message from a user and send replies
+        /// 
         /// </summary>
         /// <param name="activity"></param>
-        //[ResponseType(typeof(void))]
-        //public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
-        //{
-        //    // check if activity is of type message
-        //    if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
-        //        await Conversation.SendAsync(activity, () => new WelcomeDialog());
-        //    else
-        //        HandleSystemMessage(activity);
-
-        //    return new HttpResponseMessage(HttpStatusCode.Accepted);
-        //}
-
+        /// <returns></returns>
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            // DEMO PURPOSE: echo all incoming activities
-            Activity reply = activity.CreateReply(Newtonsoft.Json.JsonConvert.SerializeObject(activity, Newtonsoft.Json.Formatting.None));
+            var response = Request.CreateResponse(System.Net.HttpStatusCode.OK);
 
-            //var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-            //connector.Conversations.SendToConversation(reply);
+            if (activity.Type == ActivityTypes.Message
+            || activity.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Because ConversationUpdate activity is received twice 
+                // (once for the Bot being added to the conversation, and the 2nd time - 
+                // for the user), we have to filter one out, if we don't want the dialog 
+                // to get started twice. Otherwise the user will receive a duplicate message.
+                if (activity.Type == ActivityTypes.ConversationUpdate &&
+                   !activity.MembersAdded.Any(r => r.Name == "Bot"))
+                    return response;
 
-            // Process each activity
-            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new WelcomeDialog());
+                // start your root dialog here
+                await Microsoft.Bot.Builder.Dialogs.Conversation.SendAsync(activity, () =>  new WelcomeDialog());
             }
-            // Webchat: getting an "event" activity for our js code
-            else if (activity.Type == ActivityTypes.Event && activity.ChannelId == "webchat")
+            // handle other types of activity here (other than Message and ConversationUpdate 
+            // activity types)
+            else
             {
-                var receivedEvent = activity.AsEventActivity();
-
-                if ("requestWelcomeDialog".Equals(receivedEvent.Name, StringComparison.InvariantCultureIgnoreCase))
-                {
-                    // DO YOUR GREETINGS FROM HERE
-                    await Conversation.SendAsync(activity, () => new WelcomeDialog());
-                }
-            }
-            // Sample for Skype: in ContactRelationUpdate event
-            else if (activity.Type == ActivityTypes.ContactRelationUpdate && activity.ChannelId == "skype")
-            {
-                // DO YOUR GREETINGS FROM HERE
-            }
-            // Sample for emulator, to debug locales
-            else if (activity.Type == ActivityTypes.ConversationUpdate && activity.ChannelId == "emulator")
-            {
-                foreach (var userAdded in activity.MembersAdded)
-                {
-                    if (userAdded.Id == activity.From.Id)
-                    {
-                        // DO YOUR GREETINGS FROM HERE
-                    }
-                }
+                HandleSystemMessage(activity);
             }
 
-            var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
-
-        //public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
-        //{
-        //    if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
-        //    {
-        //        await Conversation.SendAsync(activity, () => new WelcomeDialog());
-        //    }
-        //    else
-        //    {
-        //        HandleSystemMessage(activity);
-        //    }
-        //    var response = Request.CreateResponse(HttpStatusCode.OK);
-        //    return response;
-        //}
 
         private async void HandleSystemMessage(Activity message)
         {
@@ -121,43 +78,7 @@ namespace Microsoft.Bot.Sample.FormBot
             }
             else if (message.Type == ActivityTypes.ConversationUpdate)
             {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-
-                //IConversationUpdateActivity update = message;
-                //var client = new ConnectorClient(new Uri(message.ServiceUrl), new MicrosoftAppCredentials());
-                //if (update.MembersAdded != null && update.MembersAdded.Any())
-                //{
-                //    foreach (var newMember in update.MembersAdded)
-                //    {
-                //        if (newMember.Id != message.Recipient.Id)
-                //        {
-                //            await Conversation.SendAsync(message, () => new WelcomeDialog());
-
-                //            var dialog = container.GetInstance<WelcomeDialog>();
-                //            await Conversation.SendAsync(message, () => dialog);
-
-                //            var reply = message.CreateReply();
-                //            reply.Text = $"Hola {newMember.Name}, soy UPECITO el asesor del Aula Virtual de UPC.Te puedo ayudar con tus consultas académicas y Técnicas del Aula Virtual.";
-
-                //            client.Conversations.ReplyToActivityAsync(reply);
-                //        }
-                //    }
-                //}
-
-                //if (message.MembersAdded.Any(o => o.Id == message.Recipient.Id))
-                //{
-                //    var bot = message.MembersAdded.FirstOrDefault();
-                //    if (bot != null && bot.Name.Equals("Upecito Bot"))
-                //    {
-                //        var dialog = container.GetInstance<RootDialog>();
-                //        await Conversation.SendAsync(message, () => dialog);
-                //    }
-                //}
-
-                //if (message.MembersRemoved.Count > 1)
-                //    await Conversation.SendAsync(message, () => new CerrarSesionDialog());
+                //  
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
