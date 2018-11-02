301775b532bd17eea44068ad8515cc7ab0798beb
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-11-02 16:54:52 -0500

    requestWelcomeDialog

--- Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
+++ Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
@@ -55,17 +55,62 @@ namespace Microsoft.Bot.Sample.FormBot
 
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
+            // DEMO PURPOSE: echo all incoming activities
+            Activity reply = activity.CreateReply(Newtonsoft.Json.JsonConvert.SerializeObject(activity, Newtonsoft.Json.Formatting.None));
+
+            var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+            connector.Conversations.SendToConversation(reply);
+
+            // Process each activity
             if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
             {
                 await Conversation.SendAsync(activity, () => new WelcomeDialog());
             }
-            else
+            // Webchat: getting an "event" activity for our js code
+            else if (activity.Type == ActivityTypes.Event && activity.ChannelId == "webchat")
             {
-                HandleSystemMessage(activity);
+                var receivedEvent = activity.AsEventActivity();
+
+                if ("requestWelcomeDialog".Equals(receivedEvent.Name, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // DO YOUR GREETINGS FROM HERE
+                    await Conversation.SendAsync(activity, () => new WelcomeDialog());
+                }
             }
+            // Sample for Skype: in ContactRelationUpdate event
+            else if (activity.Type == ActivityTypes.ContactRelationUpdate && activity.ChannelId == "skype")
+            {
+                // DO YOUR GREETINGS FROM HERE
+            }
+            // Sample for emulator, to debug locales
+            else if (activity.Type == ActivityTypes.ConversationUpdate && activity.ChannelId == "emulator")
+            {
+                foreach (var userAdded in activity.MembersAdded)
+                {
+                    if (userAdded.Id == activity.From.Id)
+                    {
+                        // DO YOUR GREETINGS FROM HERE
+                    }
+                }
+            }
+
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
+
+        //public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        //{
+        //    if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+        //    {
+        //        await Conversation.SendAsync(activity, () => new WelcomeDialog());
+        //    }
+        //    else
+        //    {
+        //        HandleSystemMessage(activity);
+        //    }
+        //    var response = Request.CreateResponse(HttpStatusCode.OK);
+        //    return response;
+        //}
 
         private async void HandleSystemMessage(Activity message)
         {
