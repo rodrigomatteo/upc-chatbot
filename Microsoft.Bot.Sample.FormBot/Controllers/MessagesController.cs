b3bc386c8d5a951a974876b0feea7d6b4040b99c
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-11-02 18:27:25 -0500

    Controller

--- Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
+++ Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
@@ -56,9 +56,9 @@ namespace Microsoft.Bot.Sample.FormBot
                 // (once for the Bot being added to the conversation, and the 2nd time - 
                 // for the user), we have to filter one out, if we don't want the dialog 
                 // to get started twice. Otherwise the user will receive a duplicate message.
-                //if (activity.Type == ActivityTypes.ConversationUpdate &&
-                //   !activity.MembersAdded.Any(r => r.Name == "Bot"))
-                //    return response;
+                if (activity.Type == ActivityTypes.ConversationUpdate &&
+                   !activity.MembersAdded.Any(r => r.Name == "Bot"))
+                    return response;
 
                 // start your root dialog here
                 await Microsoft.Bot.Builder.Dialogs.Conversation.SendAsync(activity, () =>  new WelcomeDialog());
@@ -98,9 +98,7 @@ namespace Microsoft.Bot.Sample.FormBot
             }
             else
             {
-                var reply = message.CreateReply("message.Type: " + message.Type);
-                ConnectorClient connector = new ConnectorClient(new Uri(message.ServiceUrl));
-                await connector.Conversations.ReplyToActivityAsync(reply);
+               
             }            
         }
     }
