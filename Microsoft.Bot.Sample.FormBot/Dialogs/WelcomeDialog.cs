f22b1798cea91fdce4248590a575403d7986f449
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-11-03 07:33:40 -0500

    update 1

--- Microsoft.Bot.Sample.FormBot/Dialogs/WelcomeDialog.cs
+++ Microsoft.Bot.Sample.FormBot/Dialogs/WelcomeDialog.cs
@@ -120,11 +120,15 @@ namespace FormBot.Dialogs
 
             var message = context.MakeMessage();
             var strMessage = $"Hola {userName}, soy UPECITO el asesor del Aula Virtual de UPC.Te puedo ayudar con tus consultas académicas y Técnicas del Aula Virtual.";
-            await context.PostAsync(message);
+            //await context.PostAsync(message);
+
 
             var initialText = activity.Text.ToLower();
 
             string[] choices = new string[] { "Consultas Académicas", "Consultas y Problemas Técnicos" };
+
+            await context.PostAsync(strMessage);
+            context.Wait(MessageReceivedAsync);
 
             if (initialText == "start-upecito-bot")
             {
