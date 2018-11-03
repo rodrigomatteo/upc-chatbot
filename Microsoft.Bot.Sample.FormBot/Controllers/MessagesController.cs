c1fa0d7f05cd94ce1d443f44b40baff858ea77f5
Jonathan Ramirez <jonathan.0284@hotmail.com>
2018-11-02 19:34:59 -0500

    .

--- Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
+++ Microsoft.Bot.Sample.FormBot/Controllers/MessagesController.cs
@@ -74,7 +74,7 @@ namespace Microsoft.Bot.Sample.FormBot
                 {
                     foreach (var newMember in message.MembersAdded)
                     {
-                        if (newMember.Name == "upc-chatbot")
+                        if (newMember.Name == "Bot")
                         {
                             IMessageActivity greetingMessage = Activity.CreateMessageActivity();
 
--- Microsoft.Bot.Sample.FormBot/Dialogs/WelcomeDialog.cs
+++ Microsoft.Bot.Sample.FormBot/Dialogs/WelcomeDialog.cs
@@ -78,57 +78,57 @@ namespace FormBot.Dialogs
 
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var userName = context.Activity.From.Name;
-            var userId = context.Activity.From.Id;
+            //var userName = context.Activity.From.Name;
+            //var userId = context.Activity.From.Id;
 
-            if (!string.IsNullOrEmpty(userName))
+            //if (!string.IsNullOrEmpty(userName))
+            //{
+            //    var message = context.MakeMessage();
+            //    message.Text = $"Hola {userName}, soy UPECITO el asesor del Aula Virtual de UPC.Te puedo ayudar con tus consultas académicas y Técnicas del Aula Virtual.";
+
+            //    await context.PostAsync(message);
+
+            //    var container = new Container();
+            //    DependencyResolver.UnityConfig.RegisterTypes(container);
+
+            //    var sesion = container.GetInstance<ISesion>();
+
+            //    var sesionData = sesion.CrearSesion(ConvertidorUtil.GetLong(userId));
+
+            //    if (sesionData == null)
+            //    {
+            //        var message_ = context.MakeMessage();
+            //        message.Text = "No se pudo realizar la conexión con el servidor";
+            //        await context.PostAsync(message_);
+
+            //        context.Wait(this.MessageReceived);
+            //    }
+            //    else
+            //    {
+            //        context.UserData.SetValue("sesion", sesionData);
+            //        context.Call(new MenuDialog(), ResumeWelcome);
+            //    }
+            //}
+
+            var activity = await result as Activity;
+
+            var mes = activity.Text.ToLower();
+
+            string[] choices = new string[] { "choice 1", "choice 2" };
+
+            if (Array.IndexOf(choices, mes) > -1)
             {
-                var message = context.MakeMessage();
-                message.Text = $"Hola {userName}, soy UPECITO el asesor del Aula Virtual de UPC.Te puedo ayudar con tus consultas académicas y Técnicas del Aula Virtual.";
-
-                await context.PostAsync(message);
-
-                var container = new Container();
-                DependencyResolver.UnityConfig.RegisterTypes(container);
-
-                var sesion = container.GetInstance<ISesion>();
-
-                var sesionData = sesion.CrearSesion(ConvertidorUtil.GetLong(userId));
-
-                if (sesionData == null)
-                {
-                    var message_ = context.MakeMessage();
-                    message.Text = "No se pudo realizar la conexión con el servidor";
-                    await context.PostAsync(message_);
-
-                    context.Wait(this.MessageReceived);
-                }
-                else
-                {
-                    context.UserData.SetValue("sesion", sesionData);
-                    context.Call(new MenuDialog(), ResumeWelcome);
-                }
+                await context.PostAsync($"You selected {mes}");
             }
-
-            //var activity = await result as Activity;
-
-            //var mes = activity.Text.ToLower();
-
-            //string[] choices = new string[] { "choice 1", "choice 2" };
-
-            //if (Array.IndexOf(choices, mes) > -1)
-            //{
-            //    await context.PostAsync($"You selected {mes}");
-            //}
-            //else if (mes == "show choices")
-            //{
-            //    PromptDialog.Choice(context, resumeAfterPrompt, choices, "please choose an option.");
-            //}
-            //else
-            //{
-            //    await context.PostAsync($"You sent {activity.Text} which was characters.");
-            //    context.Wait(MessageReceivedAsync);
-            //}
+            else if (mes == "show choices")
+            {
+                PromptDialog.Choice(context, resumeAfterPrompt, choices, "please choose an option.");
+            }
+            else
+            {
+                await context.PostAsync($"You sent {activity.Text} which was characters.");
+                context.Wait(MessageReceivedAsync);
+            }
 
         }
 
